ee3ff71332caeb5c2a52320adc049d1603e308ff
skrodrigo <rodrigoa0987@gmail.com>
2026-02-21 19:12:34 -0300

    feat: add sync emails script

---
+++ backend/scripts/sync-emails.mjs
@@ -0,0 +1,20 @@
+import { mkdir, readdir, copyFile } from 'node:fs/promises'
+import path from 'node:path'
+
+const distEmailsDir = path.join(process.cwd(), 'dist', 'src', 'emails')
+const srcEmailsDir = path.join(process.cwd(), 'src', 'emails')
+
+await mkdir(srcEmailsDir, { recursive: true })
+
+const files = await readdir(distEmailsDir)
+
+const jsFiles = files.filter((file) => file.endsWith('.js'))
+
+await Promise.all(
+	jsFiles.map(async (file) => {
+		await copyFile(
+			path.join(distEmailsDir, file),
+			path.join(srcEmailsDir, file)
+		)
+	})
+)
